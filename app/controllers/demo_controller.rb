06cbc5c61369e4466f57c00a177d570748056fd1
ad3adp03t <ad3adp03t@gmail.com>
2017-12-22 10:42:49 -0500

    Modified the way templates are rendered by demo_controller. learning how the controllers choose templates and possible uses with joe pullins

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,5 +2,14 @@ class DemoController < ApplicationController
 	layout false
 
   def index
+  	render(:template => 'demo/hello')
+  	#can also be typed shorthand as: render('demo/hello')
+  	#Can be shortened even further as: render('hello') since 
+  	#we are in the demo controller rails will assume we mean 
+  	#demo folder of view 
   end
+
+  def hello
+  	render('index')
+  end	
 end
