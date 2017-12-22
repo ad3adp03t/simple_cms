8832723191d0d25b008496c13589333313db8090
misturfixit <japullins420@gmail.com>
2017-12-21 20:34:54 -0500

    learning about different routing methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  # get 'demo/index'
 
+  ##  DEFAULT ROUTE...Will generate default values if no matches..things in() are optional if not used or available..catchall route not really used anymore, put at bottom if used can also add (.:format) after id to specify a format ##
+    match ':controller(/:action(/:id))', :via => :get
+  
+
+  ##  ROOT ROUTE...if no text to match routing
+  ##  root :to => "demo#index"
+  ##  or shorthand (just root&string)  
+  ##  root "demo#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
